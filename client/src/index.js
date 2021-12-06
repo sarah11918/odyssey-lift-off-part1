7137697bb9f53e1b886d415599b30bd9d1e1e1b6
Sarah Rainsberger <sarah@rainsberger.ca>
2021-12-06 17:18:04 +0000

    added apollo client and provider to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://4000-black-lizard-iuaet91b.ws-us21.gitpod.io/',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
